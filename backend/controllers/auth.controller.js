5c831920db05ab9ed2d06926813a3fe5b9499a10
yaseminnugur <yaseminnugr@gmail.com>
2026-02-11 18:26:41 +0300

    Initial commit: Server and MongoDB setup ready

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+    res.send("Sign up route called");
+}
+
+export const login = async (req, res) => {
+    res.send("login route called");
+}
+
+export const logout = async (req, res) => {
+    res.send("logout route called");
+}
