cb21f45f583fabccad02da6ecb6f65f8f620ef81
yaseminnugur <yaseminnugr@gmail.com>
2026-02-13 23:13:09 +0300

    feat: add auth middleware for protected and admin routes

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import { getAllProducts , getFeaturedProducts} from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
 
 export default router;
