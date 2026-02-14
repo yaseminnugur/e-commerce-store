ade8cc3faa189e7868e1331bba1d5be9a139d24a
yaseminnugur <yaseminnugr@gmail.com>
2026-02-14 18:05:17 +0300

    feat: implement cart management controllers for adding, removing, and updating items

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -18,6 +19,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes),
 
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);
