b129e44ecfbd545d9070dd1bc07c5262560382ec
yaseminnugur <yaseminnugr@gmail.com>
2026-02-14 22:39:59 +0300

    feat: implement coupon model and controllers for validation

---
+++ backend/models/coupon.model.js
@@ -0,0 +1,40 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const couponSchema = new mongoose.Schema({
+    code: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    discountPercentage: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100,
+    },
+    expirationDate: {
+        type: Date,
+        required: true,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        unique: true,
+    },
+
+
+},
+    {
+        timestamps: true,
+    }
+);
+
+export const Coupon = mongoose.model("Coupon", couponSchema);
+
+export default Coupon;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -19,7 +20,9 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/cart", cartRoutes),
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+
 
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);
