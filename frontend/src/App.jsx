cf8720b99c53bc0863a71884cb5d20f8a4aeca59
yaseminnugur <yaseminnugr@gmail.com>
2026-02-27 00:53:37 +0300

    feat: setup frontend structure and implement navbar component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#f5f5f7] text-gray-900 relative overflow-hidden">
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(100,129,129,0.4)_0%,rgba(80,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]" />
+        </div>
+      </div>
+
+      <div className="relative z-10 pt-20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
